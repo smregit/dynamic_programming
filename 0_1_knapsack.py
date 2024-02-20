420fefbe789d4257138dd15f70fff1cdb4e67a73
smr <samreyousef@gmail.com>
2024-02-20 16:16:06 +0530

    unbounded knapsack

--- 0_1_knapsack.py
+++ 0_1_knapsack.py
@@ -1,4 +1,6 @@
 def knapsack01(arr,capacity):
+    #when every item is selected only once
+    
     dp=[[0 for _ in range(capacity+1)] for _ in range(len(arr)+1)]
     
     
@@ -12,11 +14,28 @@ def knapsack01(arr,capacity):
                     dp[i][j]=dp[i-1][j]
                 else:
                     dp[i][j]=max(dp[i-1][j-arr[i-1][0]]+arr[i-1][1],dp[i-1][j])
+                    #cahce=max(dp[])
     for i in range(len(dp)):
         print(dp[i])
     return dp[-1][-1]
 arr=[(2,15),(5,14),(1,10),(3,45),(4,30)]
 print(knapsack01(arr,7))
+
+def knapsack23(arr,capacity):
+    #when repetition is allowed
+    dp=[0]*(capacity+1)
+    
+    for i in range(len(dp)):
+        for c in range(len(arr)):
+            if i-arr[c][0]>=0:
+                dp[i]=max(dp[i],dp[i-arr[c][0]]+arr[c][1])
+                
+                
+                
+    print(dp)
+    return dp[-1]
+print(knapsack23(arr,7))
+    
 
             
     
